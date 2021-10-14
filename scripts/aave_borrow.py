b90670ff7a98534fa8ede8b02697214de0bc9d0d
Omar Alqady <omar@founda.com>
2021-10-14 21:16:31 +0200

    feat: approve and deposit tokens

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,2 +1,42 @@
+from scripts.get_weth import get_weth
+from scripts.utils import get_account
+from brownie import network, config, interface
+from web3 import Web3
+
+AMOUNT = Web3.toWei(0.1, "ether")
+
+
 def main():
-    pass()
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+    lending_pool = get_lending_pool()
+    approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(
+        erc20_address, AMOUNT, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Deposit done!")
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
+    return tx
+
+
+def get_lending_pool():
+    # get lending pool address provider is a contract that provides
+    # the address of the lending pool
+    print("interface: ", interface)
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
