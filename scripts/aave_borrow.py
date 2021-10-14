cca25af6c7522a2eaba969dd531f0bc66747f06b
Omar Alqady <omar@founda.com>
2021-10-14 23:47:41 +0200

    feat: borrow and repay

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -19,6 +19,73 @@ def main():
     )
     tx.wait(1)
     print("Deposit done!")
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    # 95% only to avoid getting liquidated
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    print(f"We are going to borrow: {amount_dai_to_borrow} DAI")
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,  # stable interest rate
+        0,  # referral codes are deprecated
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("We borrowed some DAI")
+    get_borrowable_data(lending_pool, account)
+    repay_all(amount_dai_to_borrow, lending_pool, account)
+
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(
+        Web3.toWei(amount, "ether"),
+        lending_pool,
+        config["networks"][network.show_active()]["dai_token"],
+        account,
+    )
+    repay_tx = lending_pool.repay(
+        config["networks"][network.show_active()]["dai_token"],
+        # amount should be a little bit higher than actual amount to repay accrued
+        # interest, or use -1 to repay entire owed amount
+        amount,
+        1,
+        account.address,
+        {"from": account},
+    )
+    repay_tx.wait(1)
+    print("Repaid!")
+
+
+def get_asset_price(price_feed_address):
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    latest_price_ether = Web3.fromWei(latest_price, "ether")
+    print(f"DAI/ETH latest price: {latest_price_ether}")
+    return float(latest_price_ether)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(
+        f"Total collateral: {total_collateral_eth}\tTotal debt: {total_debt_eth}\tYou can borow: {available_borrow_eth}"
+    )
+    return (float(available_borrow_eth), float(total_debt_eth))
 
 
 def approve_erc20(amount, spender, erc20_address, account):
